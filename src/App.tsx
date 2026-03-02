6f9a20efd20e292a47375889b22e872acca4b2bf
Victor Manuel <quispequispevictormanuel1@gmail.com>
2026-03-02 15:03:16 -0500

    quitar import React no usado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import   { useState, useEffect } from "react";
 import StudyMode from "./Studymode";
 import "./styles.css";
 import Agregar from "./Agregar";
